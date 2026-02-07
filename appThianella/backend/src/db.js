df81e1f5db25ba1081e5b018e54619f96c90757a
JHayler Diaz <jhaylerzea12@gmail.com>
2026-02-06 21:24:41 -0500

    db render ready

--- appThianella/backend/src/db.js
+++ appThianella/backend/src/db.js
@@ -1,12 +1,11 @@
-const { Pool } = require('pg')
-require('dotenv').config()
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const isProduction = process.env.NODE_ENV === "production";
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
+});
 
-module.exports = pool
+module.exports = pool;
